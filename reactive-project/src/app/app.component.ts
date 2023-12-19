f028407d8083a220c805683f081b4445e3c46ec8
Jaany <Jaany@example.com>
2023-12-19 17:13:00 +0530

    first commit

---
+++ reactive-project/src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { FormGroup,FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  title = 'Angular Reactive Form';
+  post: any = '';
+  
+  loginForm = new FormGroup({
+    user:new FormControl('',[Validators.required,Validators.pattern('[a-zA-Z]+$')]),
+    password:new FormControl('',[Validators.required,Validators.minLength(5)]),
+  })
+  name: any;
+
+  loginUser(){
+    console.warn(this.loginForm.value)
+  }
+
+  get user()
+  {
+    return this.loginForm.get('user');
+  }
+
+  get password()
+  {
+    return this.loginForm.get('password');
+  }
+}
